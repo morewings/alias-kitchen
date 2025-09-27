d74cbd77730413a7717ade8094631d605fb90a6c
Dima Vyshniakov <morewings@gmail.com>
2025-09-28 01:08:55 +0200

    fix(babel): add Babel recipe and tests

--- testConfigs/babel.config.cjs
+++ testConfigs/babel.config.cjs
@@ -1,13 +1,15 @@
+const kitchen = require('./../dist/index.cjs').kitchen;
+
 module.exports = {
-    presets: ["@babel/preset-env"],
+    presets: ["@babel/preset-env", "@babel/preset-typescript"],
     plugins: [
         "@babel/plugin-transform-runtime",
         [
             // Your aliases will be under this module
             "babel-plugin-module-resolver",
             {
-                root: ["./"],
-                alias: require('src/lib/kitchen/kitchen').kitchen({recipe: 'babel'}),
+                root: ["./src"],
+                alias: kitchen({recipe: 'babel'}),
             },
         ],
     ],
